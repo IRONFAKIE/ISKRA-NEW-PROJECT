7d27b2bf1fc305f188cd489c8858904910b1ba8e
fakie <fakie1488@outlook.com>
2025-05-18 18:57:37 +0700

    приерный вид меню

--- ISKRA NEW PROJECT/Form1.Designer.cs
+++ ISKRA NEW PROJECT/Form1.Designer.cs
@@ -29,31 +29,83 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(320, 207);
+            button1.Location = new Point(268, 232);
             button1.Name = "button1";
-            button1.Size = new Size(145, 23);
+            button1.Size = new Size(254, 40);
             button1.TabIndex = 0;
-            button1.Text = "КНОПКА ПРОЕКТА";
+            button1.Text = "ИГРАТЬ";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(268, 278);
+            button2.Name = "button2";
+            button2.Size = new Size(254, 40);
+            button2.TabIndex = 1;
+            button2.Text = "НАСТРОЙКИ";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(268, 324);
+            button3.Name = "button3";
+            button3.Size = new Size(254, 40);
+            button3.TabIndex = 2;
+            button3.Text = "ВЫХОД";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(268, 370);
+            button4.Name = "button4";
+            button4.Size = new Size(254, 40);
+            button4.TabIndex = 3;
+            button4.Text = "EXTRA";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.фракталы1;
+            pictureBox1.InitialImage = Properties.Resources.фракталы;
+            pictureBox1.Location = new Point(211, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(363, 200);
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private PictureBox pictureBox1;
     }
 }
--- ISKRA NEW PROJECT/Form1.cs
+++ ISKRA NEW PROJECT/Form1.cs
@@ -11,5 +11,10 @@ namespace ISKRA_NEW_PROJECT
         {
 
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
